36413aa05d90156503464db4234c575cb9ea3344
1Victory <Living4e23@aol.com>
2015-09-08 21:34:34 -0400

    Create VIDemb.php

---
+++ VIDemb.php
@@ -0,0 +1,16 @@
+
+
+<style>
+#story{height:900px; width: 95%; border: 2px solid black;}
+#col{position: relative; float:right;  height:70%; width: 40%; border: 1px solid black;}
+#video{float:right; padding:10px; border: 2px solid black;}
+</style>
+<div id="story">
+	<div id="col"></div>
+
+
+	<div id="video"><iframe width="370" height="215" src="//www.youtube.com/embed/xeBHshlQu88" frameborder="0" allowfullscreen></iframe>
+	<iframe width="370" height="215" src="//www.youtube.com/embed/xeBHshlQu88" frameborder="0" allowfullscreen></iframe>
+	<iframe width="370" height="215" src="//www.youtube.com/embed/xeBHshlQu88" frameborder="0" allowfullscreen></iframe>
+	</div>
+</div>
